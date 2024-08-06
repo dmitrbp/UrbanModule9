c4388d7367b4b060f04b5a420e620b7c056f5459
dmitrbp <dmitrbp@jac.ru>
2024-08-06 17:18:43 +0300

    release 0

--- Module_9_3.py
+++ Module_9_3.py
@@ -1,7 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (len(x)- len(y) for x, y in zip(first, second) if len(x) - len(y) != 0)
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) - len(y) != 0)
 # second_result = (len(first[i]) == len(second[j]) for i in range(len(first)) for j in range(len(second)) if i == j)
 second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
 
